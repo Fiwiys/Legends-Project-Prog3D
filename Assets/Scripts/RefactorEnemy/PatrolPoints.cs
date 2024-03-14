b36c7c0ec480bf4536c73d2fa21fbd1241f2d361
Fiwiys <roze22ep@gmail.com>
2024-03-14 14:13:51 +0200

    Script

---
+++ Assets/Scripts/RefactorEnemy/PatrolPoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoints : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float walkSpeed;
+
+    private int currentPatrolPoint = 0;
+
+    private void Start()
+    {
+        StartCoroutine(PatrolRoutine());
+    }
+
+    private IEnumerator PatrolRoutine()
+    {
+        while (true)
+        {
+            Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, walkSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+            {
+                currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Length;
+                yield return new WaitForSeconds(1f); // Wait for a second at each patrol point
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/RefactorEnemy/RefactorEnemy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefactorEnemy : MonoBehaviour
+{
+   public Stats enemyStats;
+
+    [Tooltip("The transform that will lock onto the player once the enemy has spotted them.")]
+    public Transform sight;
+
+    [Tooltip("Blue explosion particles")]
+    public GameObject enemyExplosionParticles;
+
+    private bool slipping = false;
+    private Rigidbody rb;
+    private GameObject player;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!enemyStats.idle)
+        {
+            ChasePlayer();
+        }
+
+        if (slipping)
+        {
+            SlipBack();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        sight.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.LookAt(sight);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * enemyStats.chaseSpeed);
+
+        if (Vector3.Distance(transform.position, player.transform.position) < enemyStats.explodeDist)
+        {
+            StartCoroutine(Explode());
+            enemyStats.idle = true;
+        }
+    }
+
+    private void SlipBack()
+    {
+        transform.Translate(Vector3.back * 20 * Time.deltaTime, Space.World);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        slipping = other.gameObject.layer == 9;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            enemyStats.idle = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enemyStats.idle = true;
+        }
+    }
+
+    private IEnumerator Explode()
+    {
+        GameObject particles = Instantiate(enemyExplosionParticles, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        Destroy(transform.parent.gameObject);
+    }
+
+    [System.Serializable]
+    public struct Stats
+    {
+        [Header("Enemy Settings")]
+        [Tooltip("How fast the enemy turns in circles as they're walking (only when idle is true).")]
+        public float rotateSpeed;
+
+        [Tooltip("How fast the enemy runs after the player (only when idle is false).")]
+        public float chaseSpeed;
+
+        [Tooltip("Whether the enemy is idle or not. Once the player is within distance, idle will turn false and the enemy will chase the player.")]
+        public bool idle;
+
+        [Tooltip("How close the enemy needs to be to explode")]
+        public float explodeDist;
+    }
+}
